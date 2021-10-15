3dacc06e65a7f26e1a4bee45df24468a6b201e89
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-10-15 22:17:57 +0700

    first commit

---
+++ src/main/java/com/nguyenduc/service/IGeneralService.java
@@ -0,0 +1,15 @@
+package com.nguyenduc.service;
+
+import java.util.List;
+
+public interface IGeneralService<T> {
+    List<T> findAll();
+
+    void save(T t);
+
+    void delete(Long id);
+
+    List<T> findByName(String name);
+
+    T findByID(Long id);
+}
---
+++ src/main/java/com/nguyenduc/service/music/IMusicService.java
@@ -0,0 +1,7 @@
+package com.nguyenduc.service.music;
+
+import com.nguyenduc.model.Music;
+import com.nguyenduc.service.IGeneralService;
+
+public interface IMusicService extends IGeneralService<Music> {
+}
